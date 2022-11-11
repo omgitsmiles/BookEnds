a32985cdf9e58e53180f92d05c63c211987086a5
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-11-11 17:14:21 -0500

    added create & show to user controller
    added auth for user create and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,18 @@ class UsersController < ApplicationController
 
     def create
         user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: 201
     end
 
+    def show
+        user = User.find_by(username: session[:user_id])
+        if user
+            render json: user, status: 201
+        else
+            render json: { error: "Invalid Username or Password" }, status: 401
+    end
+    
     private
 
     def user_params
