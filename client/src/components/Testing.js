f31fe97978a2ee424e574cf19b84955defa2bc93
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-11-11 21:21:41 -0500

    install material ui
    installed material ui, w rating system

--- client/src/components/Testing.js
+++ client/src/components/Testing.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
 
 const testing = () => {
+    // const [value, setValue] = useState(2)
   return (
-    <div>testing</div>
+    <div><Typography component="legend">Controlled</Typography>
+    <Rating
+      name="simple-controlled"
+    //   value={value}
+    //   onChange={(event, newValue) => {
+    //     setValue(newValue);
+    //   }}
+    /></div>
   )
 }
 
