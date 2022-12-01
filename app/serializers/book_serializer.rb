95cd01eb3a54f2afda0dac8548d61a65bb7f75a4
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-11-30 21:16:07 -0500

    Code cleanup
    removed commented out serializer method book_reviews

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -2,16 +2,4 @@ class BookSerializer < ActiveModel::Serializer
   attributes :id, :title, :author, :genre, :description, :book_img 
   has_many :reviews
 
-  # def book_reviews
-  #     object.reviews.map do |review| 
-  #       {
-  #         id: review.id,
-  #         review: review,
-  #         rating: review.rating,
-  #         user_id: review.user.id,
-  #         username: review.user.username
-  #       }
-  #     end
-  # end
-
 end
